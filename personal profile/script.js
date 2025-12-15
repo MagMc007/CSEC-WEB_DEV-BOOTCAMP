9c5c5cef6854a875540381131651b42fb6e58f4b
MagMc007 <mergashasho7@gmail.com>
2025-12-15 07:22:43 +0300

    personal profile complete

---
+++ personal profile/script.js
@@ -0,0 +1,13 @@
+const form = document.getElementById('contactForm');
+
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Thank you, ${name}! Your message has been received.\nEmail: ${email}\nMessage: ${message}`);
+
+    form.reset();
+});
