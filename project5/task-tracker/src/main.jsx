c25df46a524c9a3c93c152bab6f607dde9b33f75
MagMc007 <mergashasho7@gmail.com>
2025-12-24 17:18:07 +0300

    set up task context

--- project5/task-tracker/src/main.jsx
+++ project5/task-tracker/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./styles/global.css";
+import { TaskProvider } from "./store/taskContext";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <TaskProvider>
+      <App />
+    </TaskProvider>
+  </BrowserRouter>
+);
